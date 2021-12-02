0a62c12c34f6a35f3ced5bc1b66829399ea194ae
Abdallah7x <76527450+Abdallah7x@users.noreply.github.com>
2021-12-02 22:46:24 +0200

    Create data.dart

---
+++ model/data.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+List menus = ["Suv", "Cross over", "Sedan", "coupe", "exiotics"];
+
+List items = [
+  {
+    "id": 1,
+    "name": "Bmw",
+    "description": "2022 Bmw 3 series",
+    "img": "assets/images/bm.jpg",
+    "p_price": "1200/day",
+    "p_promotion_price": "775/day"
+  },
+  {
+    "id": 2,
+    "name": "Honda",
+    "description": "2020 Honda CR-V",
+    "img": "assets/images/h.jpg",
+    "p_price": "800/day",
+    "p_promotion_price": "355/day"
+  },
+  {
+    "id": 3,
+    "name": "Ferrari",
+    "description": "2021 Ferrari SF-90",
+    "img": "assets/images/ff.png",
+    "p_price": "2500/day",
+    "p_promotion_price": "1400/day"
+  },
+  {
+    "id": 4,
+    "name": "verna",
+    "description": "2013 Hyundai verna",
+    "img": "assets/images/v.jpg",
+    "p_price": "300/day",
+    "p_promotion_price": "200/day"
+  },
+  {
+    "id": 5,
+    "name": "Fiat",
+    "description": "2019 Silver Fiat tipo",
+    "img": "assets/images/f.jpeg",
+    "p_price": "500/day",
+    "p_promotion_price": "350/day"
+  }
+];
