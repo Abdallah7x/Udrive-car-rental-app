180d6e2136cae75cd88a15574f4ba23707abd58c
Maryam Maged Victor <56774613+mmv2019@users.noreply.github.com>
2021-12-07 23:51:29 +0200

    Add files via upload

---
+++ lib/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xFF0D47A1);
+const Color warning = Color(0xFF0D47A1);
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF000000);
