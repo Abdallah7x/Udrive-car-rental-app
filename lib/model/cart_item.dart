af61ac155bbe42858deb6e958a5accd653430a1e
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2021-12-08 22:38:57 +0200

    Create cart_item.dart

---
+++ lib/model/cart_item.dart
@@ -0,0 +1,10 @@
+class cart_item{
+   int id;
+   String name;
+   String img;
+   String description;
+   String pPrice;
+   String pPromotionPrice;
+  
+  cart_item(this.id,this.name,this.description,this.pPromotionPrice);
+}
