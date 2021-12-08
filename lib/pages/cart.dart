859ddb1840b4fc9f21f2f5eec98ee839372b8ba3
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2021-12-08 18:32:25 +0200

    Update cart.dart

--- lib/pages/cart.dart
+++ lib/pages/cart.dart
@@ -1,35 +1,53 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:drive/model/data.dart';
 
-//import 'components/body.dart';
-//import 'components/check_out_card.dart';
 
-class cart extends StatelessWidget {
-  static String routeName = "/cart";
+class cart extends StatefulWidget{
+  @override
+  _CartViewState createState()=>_CartViewState();
+}
+
+class _CartViewState extends State<cart>{
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-      appBar: buildAppBar(context),
-     // body: Body(),
-      //bottomNavigationBar: CheckoutCard(),
-    );
-  }
-
-  AppBar buildAppBar(BuildContext context) {
-    return AppBar(
-      title: Column(
-        children: [
-          Text(
-            "Your Cart",
-            style: TextStyle(color: Colors.black),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor:Colors.transparent,
+        leading:IconButton(
+          icon: Icon(
+            Icons.keyboard_arrow_left,
+            color:Colors.blue,
           ),
-          Text(
-            "${items.length} items",
-            style: Theme.of(context).textTheme.caption,
+          onPressed: (){},
+          ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child:Column(
+        children:<Widget>[
+          Text("your cart",
+          style:TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 21.0,
+          ),
+          ),
+          SizedBox(height: 12.0),
+          Container(
+            width:80.0,
+            height: 80.0,
+            decoration: BoxDecoration(
+            color:Colors.red[100],
+            borderRadius: BorderRadius.circular(20.0),
+          ),
           ),
         ],
       ),
+      ),
     );
   }
+  
 }
-
