49749b85215421149607d69cad3de8ec37a58435
Pierre Malak <69065062+Pierre-Malak@users.noreply.github.com>
2021-12-09 22:49:33 +0200

    Update users.dart

--- lib/model/users.dart
+++ lib/model/users.dart
@@ -1,10 +1,11 @@
-import 'package:flutter_note_app_provider/models/data.dart';
+//import 'package:flutter_note_app_provider/models/data.dart';
+import 'package:flutter/material.dart';
 class Users {
   String name;
   String id;
   String email;
   String img;
 
-  Notes(this.name, this.id, this.email, this.img);
+  Users(this.name, this.id, this.email, this.img);
  
 }
