c4ca61b46aa3d7d7635883fcaa8d3f8238b970ee
Pierre Malak <69065062+Pierre-Malak@users.noreply.github.com>
2021-12-09 22:44:45 +0200

    Create users.dart

---
+++ lib/model/users.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_note_app_provider/models/data.dart';
+class Users {
+  String name;
+  String id;
+  String email;
+  String img;
+
+  Notes(this.name, this.id, this.email, this.img);
+ 
+}
