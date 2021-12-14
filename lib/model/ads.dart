08e6945f47a99509adb0172f7c8cf2e6a71f7f5a
Maryam Maged Victor <56774613+mmv2019@users.noreply.github.com>
2021-12-14 02:24:23 +0200

    Add files via upload

---
+++ lib/model/ads.dart
@@ -0,0 +1,8 @@
+class Ads {
+  String car_name;
+  String car_number;
+  String title;
+  String price;
+
+  Ads(this.car_name, this.car_number, this.title, this.price);
+}
---
+++ lib/model/cars.dart
@@ -0,0 +1,9 @@
+
+class Car {
+  String name;
+  String number;
+  String type;
+  String rate;
+
+  Car(this.name, this.number, this.type, this.rate);
+}
