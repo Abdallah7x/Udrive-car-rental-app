14380b3430f3f170d800cdcb2a4536b33c508bb4
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2021-12-14 19:26:13 +0200

    Update cart.dart

--- lib/pages/cart.dart
+++ lib/pages/cart.dart
@@ -1,53 +1,52 @@
-import 'dart:html';
+// ignore_for_file: use_key_in_widget_constructors
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:drive/providers/cart_functions.dart';
+import 'package:provider/provider.dart';
 import 'package:drive/model/data.dart';
+import 'home_page.dart';
 
 
-class cart extends StatefulWidget{
+
+class CheckoutPage extends StatefulWidget {
   @override
-  _CartViewState createState()=>_CartViewState();
+ 
+  _CheckoutPageState createState() => _CheckoutPageState();
 }
 
-class _CartViewState extends State<cart>{
+class _CheckoutPageState extends State<CheckoutPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        backgroundColor:Colors.transparent,
-        leading:IconButton(
-          icon: Icon(
-            Icons.keyboard_arrow_left,
-            color:Colors.blue,
-          ),
-          onPressed: (){},
-          ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child:Column(
-        children:<Widget>[
-          Text("your cart",
-          style:TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 21.0,
-          ),
-          ),
-          SizedBox(height: 12.0),
-          Container(
-            width:80.0,
-            height: 80.0,
-            decoration: BoxDecoration(
-            color:Colors.red[100],
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          ),
-        ],
-      ),
-      ),
+    return Consumer<CartProvider>(
+      builder: (context, CartProvider, child) {
+        return Scaffold(
+         
+            appBar: AppBar(
+              title: Text('Checkout Page [\$ ${CartProvider.totalPrice}]'),
+            ),
+            body: CartProvider.basketItems.length == 0
+                ? Text('no items in your cart')
+                : ListView.builder(
+                    itemCount: CartProvider.basketItems.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          title: Text("hi"),
+                            // CartProvider.basketItems[index].name),
+                          subtitle: Text("hi"),
+                              // Text(CartProvider.basketItems[index].pPromotionPrice.toString()),
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              CartProvider.remove(CartProvider.basketItems[index]);
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  ));
+      },
     );
   }
-  
 }
