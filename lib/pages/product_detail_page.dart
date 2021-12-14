e0b574f43bcb6f1c4ada5f1c7a643049b2cff910
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2021-12-14 19:24:52 +0200

    Update product_detail_page.dart

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:drive/theme/colors.dart';
 import 'package:drive/model/data.dart';
 import 'cart.dart';
+import 'user_profile.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart_functions.dart';
+import 'item.dart';
+
 
 class ProductDetailPage extends StatefulWidget {
+  
   final int id;
   final String name;
   final String img;
@@ -12,27 +18,42 @@ class ProductDetailPage extends StatefulWidget {
   final String pPrice;
   final String pPromotionPrice;
 
+  final Item item;
+
   ProductDetailPage(
-      {this.id,
+      {
+        this.item,
+        this.id,
       this.name,
       this.img,
       this.description,
       this.pPrice,
-      this.pPromotionPrice});
+      this.pPromotionPrice}){
+        print(id);
+      }
   @override
   _ProductDetailPageState createState() => _ProductDetailPageState();
 }
 
 class _ProductDetailPageState extends State<ProductDetailPage> {
   int qty = 1;
+  int activeIndex = 0;
+
+  // Item get item => null;
   @override
   Widget build(BuildContext context) {
+     return Consumer<CartProvider>(
+      builder:(context,CartProvider data,child){
+    Item items;
     return Scaffold(
-      body: getBody(),
+      
+      body: getBody(items),
     );
+       }
+    ); 
   }
 
-  Widget getBody() {
+  Widget getBody(Item items) {
     return SafeArea(
       child: ListView(
         children: <Widget>[
@@ -192,7 +213,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                         setState(() {
                           qty = ++qty;
                         });
-                        // minus icon
+                        // minus here
                       },
                       child: Container(
                         width: 25,
@@ -211,26 +232,291 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
             height: 50,
           ),
           InkWell(
-            onDoubleTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => cart()),
-              );
-            },
-            child: Center(
-                child: ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.blue.shade900),
-              ),
-              child: Text(
+            child: Text(
                 'Rent it',
               ),
-              onPressed: () {},
-            )),
-          )
+            onDoubleTap: () {
+              
+               Provider.of<CartProvider>(context, listen: false).add(items);
+              // context.read<CartProvider>().add(items[index].name);
+              Navigator.push(
+
+                context,
+                MaterialPageRoute(builder: (context) => CheckoutPage()),
+              );
+        //          child: Center(
+        //         child: 
+        //             ListView.builder(
+                      
+        //     shrinkWrap: true,          
+        //   itemCount: items.length,
+        //   itemBuilder: (context, index) {
+        //     return ListTile(
+              
+        //       // title: Text(items[index].name),
+        //       // subtitle: Text(items[index].pPromotionPrice.toString()),
+        //       // trailing: Icon(Icons.add),
+        //       onTap: () {
+        //          setState(() {
+        //                       activeIndex = index;
+        //                        id: items[index]['id'];
+        //                        name: items[index]['name'];
+        //                       // context.read<CartProvider>().add(items[index].name);
+        //                       print(items[index]
+        //                       );
+        //                       Provider.of<CartProvider>(context, listen: false).add(items[index]);
+        //                     });
+                
+        //         // cart_functions.add(items[index]);
+        //       },
+        //     );
+        //   },
+        // )
+            
+          
+        //   );
+            },
+         
+            ),
         ],
       ),
     );
   }
 }
+
+// class ProductDetialedPages extends StatelessWidget{
+//   Widget build(BuildContext context){
+//      return Scaffold(
+      
+//       body: getBody(item, context ),
+//     );
+//        }
+//     ); 
+//   }
+
+//   Widget getBody(Item item, BuildContext context) {
+//     return SafeArea(
+//       child: ListView(
+//         children: <Widget>[
+//           InkWell(
+//             onTap: () {
+//               Navigator.pop(context);
+//             },
+//             child: Padding(
+//               padding: EdgeInsets.only(left: 20),
+//               child: Align(
+//                   alignment: Alignment.centerLeft,
+//                   child: Icon(Icons.arrow_back_ios)),
+//             ),
+//           ),
+//           Hero(
+//             tag: widget.id.toString(),
+//             child: Container(
+//               width: 400,
+//               height: 400,
+//               decoration: BoxDecoration(
+//                   image: DecorationImage(
+//                       image: AssetImage(widget.img), fit: BoxFit.cover)),
+//             ),
+//           ),
+//           SizedBox(
+//             height: 50,
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.only(left: 20, right: 20),
+//             child: Row(
+//               children: <Widget>[
+//                 Text(
+//                   "Name :",
+//                   style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+//                 ),
+//                 SizedBox(
+//                   width: 20,
+//                 ),
+//                 // Text(
+//                 //   widget.name,
+//                 //   style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+//                 // )
+//               ],
+//             ),
+//           ),
+//           SizedBox(
+//             height: 20,
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.only(left: 20, right: 20),
+//             child: Row(
+//               children: <Widget>[
+//                 Text(
+//                   "description :",
+//                   style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+//                 ),
+//                 SizedBox(
+//                   width: 20,
+//                 ),
+//                 Text(
+//                   widget.description,
+//                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+//                 )
+//               ],
+//             ),
+//           ),
+//           SizedBox(
+//             height: 20,
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.only(left: 20, right: 20),
+//             child: Row(
+//               children: <Widget>[
+//                 Text(
+//                   "Price :",
+//                   style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+//                 ),
+//                 SizedBox(
+//                   width: 20,
+//                 ),
+//                 Row(
+//                   children: <Widget>[
+//                     // Text(
+//                     //   // widget.pPromotionPrice + " USD",
+//                     //   style:
+//                     //       TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+//                     // ),
+//                     SizedBox(
+//                       width: 20,
+//                     ),
+//                     // Text(
+//                     //   // widget.pPrice + " USD",
+//                     //   style: TextStyle(
+//                     //       fontSize: 18,
+//                     //       fontWeight: FontWeight.w500,
+//                     //       color: warning,
+//                     //       decoration: TextDecoration.lineThrough),
+//                     // )
+//                   ],
+//                 )
+//               ],
+//             ),
+//           ),
+//           SizedBox(
+//             height: 25,
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.only(left: 20, right: 20),
+//             child: Row(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//             ),
+//           ),
+//           SizedBox(
+//             height: 10,
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.only(right: 20, left: 20),
+//             child: Row(
+//               children: <Widget>[
+//                 Text(
+//                   "Number of days:",
+//                   style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+//                 ),
+//                 SizedBox(
+//                   width: 20,
+//                 ),
+//                 Row(
+//                   children: <Widget>[
+//                     InkWell(
+//                       // onDoubleTap: () {
+//                       //   if (qty > 1) {
+//                       //     setState(() {
+//                       //       qty = --qty;
+//                       //     });
+//                       //   }
+//                       // },
+//                       child: Container(
+//                         width: 25,
+//                         height: 25,
+//                         decoration: BoxDecoration(
+//                             border: Border.all(color: primary),
+//                             shape: BoxShape.circle),
+//                       ),
+//                     ),
+//                     SizedBox(
+//                       width: 15,
+//                     ),
+//                     Text(
+//                       // qty.toString(),
+//                       style: TextStyle(fontSize: 16),
+//                     ),
+//                     SizedBox(
+//                       width: 15,
+//                     ),
+//                     InkWell(
+//                       // onDoubleTap: () {
+//                       //   setState(() {
+//                       //     qty = ++qty;
+//                       //   });
+//                       //   // minus here
+//                       // },
+//                       child: Container(
+//                         width: 25,
+//                         height: 25,
+//                         decoration: BoxDecoration(
+//                             border: Border.all(color: primary),
+//                             shape: BoxShape.circle),
+//                       ),
+//                     ),
+//                   ],
+//                 )
+//               ],
+//             ),
+//           ),
+//           SizedBox(
+//             height: 50,
+//           ),
+//           InkWell(
+//             child: Text(
+//                 'Rent it',
+//               ),
+//             onDoubleTap: () {
+//               Provider.of<CartProvider>(context, listen: false).add(item);
+//               Navigator.push(
+
+//                 context,
+//                 MaterialPageRoute(builder: (context) => CheckoutPage()),
+//               );
+//                  child: Center(
+//                 child: 
+//                     ListView.builder(
+                      
+//             shrinkWrap: true,          
+//           itemCount: items.length,
+//           itemBuilder: (context, index) {
+//             return ListTile(
+              
+//               // title: Text(items[index].name),
+//               // subtitle: Text(items[index].pPromotionPrice.toString()),
+//               // trailing: Icon(Icons.add),
+//               onTap: () {
+//                                id: items[index]['id'];
+//                                name: items[index]['name'];
+//                               // context.read<CartProvider>().add(items[index].name);
+//                               print(items[index]
+//                               );
+//                               Provider.of<CartProvider>(context, listen: false).add(items[index]);
+//                             });
+                
+//                 // cart_functions.add(items[index]);
+//                       );
+//           },
+//         )
+            
+          
+//           );
+//             },
+         
+//             ),
+//         ],
+//       ),
+//     );
+//   }
+//   }
+// }
