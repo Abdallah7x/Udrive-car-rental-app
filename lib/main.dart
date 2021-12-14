8ec49255a40b33d3762db6287537ae51ad3c5482
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2021-12-14 19:28:29 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 import 'pages/login.dart';
+import 'package:provider/provider.dart';
+import 'providers/cart_functions.dart';
+import 'pages/home_page.dart';
+import 'pages/user_profile.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-  ));
+  // runApp(MaterialApp(
+  //   home: HomePage(),
+  // ));
+  runApp(
+   MyApp()
+  // MaterialApp(home: Text("NEW"))
+  );
+}
+
+class MyApp extends StatelessWidget{
+  build(BuildContext context){
+    return  ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: MaterialApp(home: Scaffold(body: productPage())),
+      );
+  }
 }
