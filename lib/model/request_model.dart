9edd132173b0a4f578ab6a83bf3dcd74318df9fb
Abdallah7x <76527450+Abdallah7x@users.noreply.github.com>
2022-01-21 15:21:56 +0200

    Add files via upload

---
+++ lib/model/request_model.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+class request {
+  final DateTime dateTime;
+  final DateTime dateTimee;
+  final File file;
+  final File file2;
+  final File file3;
+
+  request(
+    this.dateTime,
+    this.dateTimee,
+    this.file,
+    this.file2,
+    this.file3,
+  );
+}
