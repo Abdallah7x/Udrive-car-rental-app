0b959c14b81ca72637ae20da4be45ae0023ed1ee
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2022-01-22 04:40:27 +0200

    Update product_page.dart

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,229 +1,192 @@
-import 'package:drive/pages/user_profile.dart';
 import 'package:flutter/material.dart';
-import 'package:drive/model/data.dart';
-import 'package:drive/theme/colors.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'product_detail_page.dart';
-import 'cart.dart';
-import 'package:provider/provider.dart';
-import '../providers/cart_functions.dart';
+import 'package:drive/pages/userprofile.dart';
+import 'package:drive/pages/login1.dart';
+import 'package:drive/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class productPage extends StatefulWidget {
   @override
-  _productPageState createState() => _productPageState();
+  _productPage createState() => _productPage();
 }
 
-class _productPageState extends State<productPage> {
+class _productPage extends State<productPage> {
+  User user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user.uid)
+        .get()
+        .then((value) {
+      this.loggedInUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
+  }
+
   int activeIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-   // return Consumer<CartProvider>(
-      //builder:(context,cart,child){
-        return  Scaffold(
-          appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
         actions: <Widget>[
+          Row(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.account_circle),
+                    onPressed: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) => r()));
+                    },
+                  ),
+                  // IconButton(
+                  //   icon: Icon(Icons.shopping_cart),
+                  //   onPressed: () {
+                  //     Navigator.of(context).push(MaterialPageRoute(
+                  //         builder: (context) => CheckoutPage()));
+                  //   },
+                  // ),
+                  Text('3'),
+                  SizedBox(
+                    width: 10,
+                  ),
+                ],
+              )
+            ],
+          ),
           FlatButton(
             textColor: Colors.white,
             onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => MyLogin()),
-              );
+              logout(context);
             },
             child: Text("Log out"),
             shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
           ),
         ],
         backgroundColor: Colors.blue.shade900,
-        centerTitle: true,
         title: Text(
-          'Our fleet',
+          "Hey ${loggedInUser.firstName}!",
         ),
       ),
-          body: getBody(),
-         );
-      //}
-   // );
-  }
-
-  Widget getBody() {
-    return SafeArea(
-      child: ListView(
-        padding: EdgeInsets.only(left: 20, right: 20),
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                "Rental cars",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w500,
-                  color: primary,
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    icon:Icon(Icons.account_circle),
-                     onPressed: () { 
-                       
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => userprofile()));
-                    
-                      },
-                      
-
-                  ),
-                  
-                   
-                  IconButton(
-                  icon:Icon(Icons.shopping_cart),
-                     onPressed: () { 
-                       
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => CheckoutPage()));
-                    
-                      },
-                   ),
-                   Text('3'),
-                  SizedBox(
-                    width: 10,
-                  ),
-
-                ],
-              )
-            ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-                children: List.generate(
-                    menus.length,
-                    (index) => InkWell(
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('cars').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: Image(
+                  image: NetworkImage(
+                      'https://www.pricelesscarrental.com/smsite/npauto_5/media/imgs/6125_loader.gif')),
+            );
+          }
+          return ListView(
+            children: snapshot.data.docs.map((document) {
+              return Column(
+                  children: List.generate(
+                      1,
+                      (index) => InkWell(
                           onTap: () {
-                            setState(() {
-                              activeIndex = index;
-                            });
+                            print(document['id']);
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => ProductDetailPage(
+                                          // item: items[index],
+
+                                          id: document['id'],
+                                          name: document['name'],
+                                          img: document['img'],
+                                          description: document['description'],
+                                          pPrice:
+                                              document['p_price'].toString(),
+                                          pPromotionPrice:
+                                              document['p_promotion_price']
+                                                  .toString(),
+                                        )));
                           },
-                          child: Padding(
-                            padding: const EdgeInsets.only(right: 20),
-                            child: Container(
-                              height: 40,
-                              decoration: BoxDecoration(
-                                  color: activeIndex == index
-                                      ? primary
-                                      : Colors.transparent,
-                                  border: Border.all(
-                                      color: activeIndex == index
-                                          ? Colors.transparent
-                                          : primary),
-                                  borderRadius: BorderRadius.circular(5)),
-                              child: Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Text(
-                                  menus[index],
-                                  style: TextStyle(
-                                      color: activeIndex == index
-                                          ? white
-                                          : primary,
-                                      fontSize: 16),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ))),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Column(
-            children: List.generate(
-                items.length,
-                (index) => InkWell(
-                      onTap: () {
-                        print(items[index]['id']);
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (_) => ProductDetailPage(
-                                  // item: items[index],
-                      
-                                      id: items[index]['id'],
-                                      name: items[index]['name'],
-                                      img: items[index]['img'],
-                                      description: items[index]['description'],
-                                      pPrice:
-                                          items[index]['p_price'].toString(),
-                                      pPromotionPrice: items[index]
-                                              ['p_promotion_price']
-                                          .toString(),
-                                    )));
-                      },
-                      child: Card(
-                        child: Row(
-                          children: <Widget>[
-                            Hero(
-                              tag: items[index]['id'].toString(),
-                              child: Container(
-                                height: 120,
-                                width: 120,
-                                decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        image: AssetImage(items[index]['img']),
-                                        fit: BoxFit.cover)),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                          child: Card(
+                            child: Row(
                               children: <Widget>[
-                                Text(
-                                  items[index]['name'],
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500),
+                                Hero(
+                                  tag: document['id'].toString(),
+                                  child: Container(
+                                    height: 120,
+                                    width: 120,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image:
+                                                NetworkImage(document['img']),
+                                            fit: BoxFit.cover)),
+                                  ),
                                 ),
                                 SizedBox(
-                                  height: 20,
+                                  width: 20,
                                 ),
-                                Row(
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget>[
                                     Text(
-                                      items[index]['p_promotion_price']
-                                          .toString(),
+                                      document['name'],
                                       style: TextStyle(
-                                        fontSize: 16,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 20,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w500),
                                     ),
                                     Text(
-                                      items[index]['p_price'].toString(),
+                                      document['description'],
                                       style: TextStyle(
-                                          fontSize: 17,
-                                          color: warning,
-                                          fontWeight: FontWeight.w500,
-                                          decoration:
-                                              TextDecoration.lineThrough),
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w500),
                                     ),
+                                    SizedBox(
+                                      height: 20,
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Text(
+                                          document['p_promotion_price']
+                                              .toString(),
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 20,
+                                        ),
+                                        Text(
+                                          document['p_price'].toString(),
+                                          style: TextStyle(
+                                              fontSize: 17,
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.w500,
+                                              decoration:
+                                                  TextDecoration.lineThrough),
+                                        ),
+                                      ],
+                                    )
                                   ],
                                 )
                               ],
-                            )
-                          ],
-                        ),
-                      ),
-                    )),
-          )
-        ],
+                            ),
+                          ))));
+            }).toList(),
+          );
+        },
       ),
     );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.remove('email');
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()));
   }
 }
 
