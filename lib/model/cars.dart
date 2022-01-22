0b941ce936ef44dbee456da10455389b4eea5054
Pierre Malak <69065062+Pierre-Malak@users.noreply.github.com>
2022-01-22 11:12:57 +0200

    Update cars.dart

--- lib/model/cars.dart
+++ lib/model/cars.dart
@@ -1,9 +1,21 @@
-
 class Car {
+  String id;
   String name;
   String number;
   String type;
   String rate;
 
-  Car(this.name, this.number, this.type, this.rate);
+  Car({this.id, this.name, this.number, this.type, this.rate});
+
+
+  factory Car.fromJson(Map<String, dynamic> json) {
+    return Car(
+      id: json['id'],
+      name: json['data']['name'],
+      number: json['data']['number'],
+      type: json['data']['type'],
+      rate: json['data']['rate'],
+    );
+  }
+
 }
