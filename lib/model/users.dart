2efbd27d9f0d0180ad4c581503b9c3c1b40cf57f
Pierre Malak <69065062+Pierre-Malak@users.noreply.github.com>
2022-01-22 11:12:10 +0200

    Update users.dart

--- lib/model/users.dart
+++ lib/model/users.dart
@@ -1,11 +1,21 @@
 //import 'package:flutter_note_app_provider/models/data.dart';
+import 'package:drive/pages/password.dart';
 import 'package:flutter/material.dart';
+
 class Users {
-  String name;
+  String firstname;
   String id;
   String email;
-  String img;
+  String secondname;
 
-  Users(this.name, this.id, this.email, this.img);
- 
+  Users({this.firstname,this.secondname, this.id, this.email});
+
+  factory Users.fromJson(Map<String, dynamic> json) {
+    return Users(
+      id: json['id'],
+      firstname: json['data']['firstName'],
+      email: json['data']['email'],
+      secondname: json['data']['secondName'],
+    );
+  }
 }
